a934747cd16dadeae1dc7b17560a1b307557bf2a
Wagner Andrade <wsawebmaster@yahoo.com.br>
2025-09-02 11:45:48 +0000

    feat: lendo json

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,9 +1,12 @@
 import { ClubModel } from "../models/club-model";
+import fs from "fs/promises";
 
-const database = [
-        { id: 1, name: "Real Madrid" }
-    ];
+// const database = [
+//         { id: 1, name: "Real Madrid" }
+//     ];
 
 export const findAllClubs = async (): Promise<ClubModel[]> => {
-    return database;
+    const data = await fs.readFile("./src/data/clubs.json", "utf-8");
+    const clubs: ClubModel[] = JSON.parse(data);
+    return clubs;
 }
