853d6aafba9b6d3720578608ec0bb5522a5deb79
Wagner Andrade <wsawebmaster@yahoo.com.br>
2025-09-02 11:30:01 +0000

    feat: criado recurso clubs

---
+++ src/repositories/clubs-repository.ts
@@ -0,0 +1,9 @@
+import { ClubModel } from "../models/club-model";
+
+const database = [
+        { id: 1, name: "Real Madrid" }
+    ];
+
+export const findAllClubs = async (): Promise<ClubModel[]> => {
+    return database;
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as PlayerController from './controllers/players-controller';
+import * as ClubController from './controllers/clubs-controller';
 
 const router = Router();
 
@@ -7,7 +8,8 @@ router.get("/players", PlayerController.getPlayer);
 router.post("/players", PlayerController.postPlayer);
 router.delete("/players/:id", PlayerController.deletePlayer);
 router.patch("/players/:id", PlayerController.updatePlayer);
-
 router.get("/players/:id", PlayerController.getPlayerById);
+
+router.get("/clubs", ClubController.getClubs);
 
 export default router;
