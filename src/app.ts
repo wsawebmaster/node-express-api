624bcbdc4854ad2a05287d4cf4b5217f66ea3d94
Wagner Andrade <wsawebmaster@yahoo.com.br>
2025-09-02 12:00:42 +0000

    feat: implementado cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import router from './routes';
+import cors from "cors";
 
 function createApp() {
   const app = express();
 
   app.use(express.json());
   app.use("/api", router);
+  app.use(cors());
 
   return app;
 }
